559254821c28214e74102cf6f800e9daf4176362
wassimmatous <wassim.mattous@gmail.com>
2021-10-22 10:40:29 -0400

    cli code formatting

--- Cli.py
+++ Cli.py
@@ -1,17 +1,10 @@
 from __future__ import print_function, unicode_literals
-
 import json
 import re
 import threading
 import time
-
-import inquirer
 import requests
-from PyInquirer import prompt
-from PyInquirer import style_from_dict, Token
-
 from PyInquirer import style_from_dict, Token, prompt, Separator
-from pprint import pprint
 from Upcloud_API import Upcloud_API
 from shell import Shell
 import logs
@@ -35,39 +28,18 @@ class Cli:
         self.manager = Upcloud_API()
         self.mylogger = logs.Logs()
 
-
-
-    def multi_choice2(self):
-        vm_list=self.get_all_servers_list()
-        vm_dict_list=[]
-        for i in vm_list:
-            info = {
-                "name": i
-            }
-            vm_dict_list.append(info)
-        questions = [
-            {
-                'type': 'checkbox',
-                'message': 'Please choose the VMs you want to delete(you need to to choose at least one)',
-                'name': 'vm_list_to_delete',
-                'choices': vm_dict_list,
-                'validate': lambda answer: 'You must choose at least one VM.' \
-                    if len(answer) == 0 else True
-            }
-        ]
-        answers = prompt(questions, style=style)
-        return answers['vm_list_to_delete']
-
+    # ask the user to choose an action from the action list
     def ask_action(self):
         directions_prompt = {
             'type': 'list',
             'name': 'action',
             'message': 'Which action would you like to perform?',
-            'choices': ['CreateVM', 'CheckVmStatus', 'DeleteVm', 'VmConsole', 'PerformanceStat', 'VmEvents', 'Exit']
+            'choices': ['Create VM', 'Check VM Status', 'Delete VM', 'Access VM Console', 'Check VM Performance', 'Check VM Events', 'EXIT']
         }
         answers = prompt(directions_prompt)
         return answers['action']
 
+    # ask the user the choose a zone when creating a vm
     def ask_zone(self):
         response = requests.get(baseURL + '/zone')
         zones = response.json()
@@ -80,6 +52,7 @@ class Cli:
         answers = prompt(directions_prompt)
         return answers['zone']
 
+    # ask the user the choose a plan when creating a vm
     def ask_plan(self):
         response = requests.get(baseURL + '/plan')
         plans = response.json()
@@ -92,6 +65,7 @@ class Cli:
         answers = prompt(directions_prompt)
         return answers['plan'], plans[answers['plan']]
 
+    # ask the user the choose an os when creating a vm
     def ask_os(self):
         directions_prompt = {
             'type': 'list',
@@ -102,6 +76,7 @@ class Cli:
         answers = prompt(directions_prompt)
         return answers['os']
 
+    # ask the user the choose  to change the os size when creating a vm
     def ask_os_size(self, default_size):
         directions_prompt = {
             'type': 'list',
@@ -112,6 +87,7 @@ class Cli:
         answers = prompt(directions_prompt)
         return answers['os_size']
 
+    # get the updated os size
     def get_os_storage(self):
         while True:
             try:
@@ -125,8 +101,7 @@ class Cli:
                 continue
         return os_st
 
-    # TODO better to use while loop than recursion!
-
+    # ask the user if he would like to monitor the request
     def request_progress(self):
         directions_prompt = {
             'type': 'list',
@@ -137,21 +112,26 @@ class Cli:
         answers = prompt(directions_prompt)
         return answers['request_prog']
 
-    def choice_confirm(self):
-        directions_prompt = {
-            'type': 'list',
-            'name': 'confirm_option',
-            'message': 'please confirm your options?',
-            'choices': ['Confirm', 'Choose again', 'Return To the Main Menu']
-        }
-        answers = prompt(directions_prompt)
-        if answers['confirm_option'] == "Confirm":
-            print("options confirmed\n")
-        elif answers['confirm_option'] == "Choose again":
-            self.get_vm_details()
-        elif answers['confirm_option'] == "Return To the Main Menu":
-            self.action()
 
+    # utility function
+    def get_os_dict(self):
+        response = requests.get(baseURL + '/template')
+        mylist = response.json()
+        all_keys = list(set().union(*(d.keys() for d in mylist)))
+        all_values = list(set().union(*(d.values() for d in mylist)))
+        d = dict(zip(all_keys, all_values))
+        return d
+
+    #get the existing server list
+    def get_all_servers_list(self):
+        hostname_list = []
+        response = requests.get(baseURL + '/server')
+        for i in response.json():
+            hostname_list.append(i['hostname'] + " : " + i['uuid'])
+
+        return hostname_list
+
+    # get details for a specific vm
     def get_vm_details(self):
         vmDetails = []
         zone = self.ask_zone()
@@ -179,43 +159,33 @@ class Cli:
             count = count + 1
         return vmDetails
 
-    def get_os_dict(self):
-        response = requests.get(baseURL + '/template')
-        mylist = response.json()
-        all_keys = list(set().union(*(d.keys() for d in mylist)))
-        all_values = list(set().union(*(d.values() for d in mylist)))
-        d = dict(zip(all_keys, all_values))
-        return d
+    #get server status using it uuid
+    def get_server_status(self, uuid):
+        response = requests.get(baseURL + '/server/status/' + uuid)
+        return response.text
 
-    def get_all_servers_list(self):
-        hostname_list = []
-        response = requests.get(baseURL + '/server')
-        for i in response.json():
-            hostname_list.append(i['hostname'] + ":" + i['uuid'])
+    #utility function to get user input
+    def get_input(self, msg):
+        questions = [
+            {
+                'type': 'input',
+                'name': 'x',
+                'message': msg,
+            }
+        ]
+        if 'title' not in msg:
+            while True:
+                answers = prompt(questions)
+                if answers['x']:
+                    break
+                else:
+                    print('Please enter a value.')
+        else:
+            answers = prompt(questions)
+        return answers['x']
 
-        return hostname_list
 
-    #  def get_delete_choice(self):
-    #      question = {
-    #          'type': 'list',
-    #          'name': 'delete_choice',
-    #          'message': '  please pick a choice for the next step?',
-    #          'choices': ['choice from existing VMs list','enter a hostname','enter VM UUID ']
-    #      }
-    #      answers = prompt(question)
-    #      return answers['delete_choice']
-    #
-    #
-    # def pick_vm(self):
-    #     question1 = {
-    #         'type': 'list',
-    #         'name': 'vm_choice',
-    #         'message': ' please pick one of the bellow VM list ',
-    #         'choices': self.get_all_servers_list()
-    #     }
-    #     answer1 = prompt(question1)
-    #     return answer1['vm_choice']
-
+    # ask the user to pick a vm from the existing vm list
     def pick_vm(self):
 
         directions_prompt = {
@@ -227,6 +197,7 @@ class Cli:
         answers = prompt(directions_prompt)
         return answers['vm']
 
+    # get the user chice
     def get_choice(self,action='not_multi_choices'):
         directions_prompt = {
             'type': 'list',
@@ -239,7 +210,7 @@ class Cli:
             if action == 'multi_choices':
                 return self.multi_choice2()
             else:
-                return self.pick_vm().split(':')[1]
+                return self.pick_vm().split(':')[1].strip()
 
         elif answers['delete_option'] == "enter VM UUID":
             count = 0
@@ -259,6 +230,45 @@ class Cli:
             print('########EXITING PROGRAM THANKS##########')
             exit()
 
+    # get muliti_choice for the user
+    def multi_choice2(self):
+        vm_list=self.get_all_servers_list()
+        vm_dict_list=[]
+        for i in vm_list:
+            info = {
+                "name": i
+            }
+            vm_dict_list.append(info)
+        questions = [
+            {
+                'type': 'checkbox',
+                'message': 'Please choose the VMs you want to delete(you need to to choose at least one)',
+                'name': 'vm_list_to_delete',
+                'choices': vm_dict_list,
+                'validate': lambda answer: 'You must choose at least one VM.' \
+                    if len(answer) == 0 else True
+            }
+        ]
+        answers = prompt(questions, style=style)
+        return answers['vm_list_to_delete']
+
+    # confirm the user options when creating the vm
+    def choice_confirm(self):
+        directions_prompt = {
+            'type': 'list',
+            'name': 'confirm_option',
+            'message': 'please confirm your options?',
+            'choices': ['Confirm', 'Choose again', 'Return To the Main Menu']
+        }
+        answers = prompt(directions_prompt)
+        if answers['confirm_option'] == "Confirm":
+            print("options confirmed\n")
+        elif answers['confirm_option'] == "Choose again":
+            self.get_vm_details()
+        elif answers['confirm_option'] == "Return To the Main Menu":
+            self.action()
+
+    #get vm status
     def get_checkStatus_choice(self):
         directions_prompt = {
             'type': 'list',
@@ -282,11 +292,13 @@ class Cli:
             print('########EXITING PROGRAM THANKS##########')
             exit()
 
+    #  check all vms status
     def check_all_vms_status(self):
         response = requests.get(baseURL + '/server')
         for server in response.json():
-            print(server['hostname'] + ":" + server['uuid'] + ":" + server['state'])
+            print(server['hostname'] + " : " + server['uuid'] + " ===> " + server['state'])
 
+    # return vm details just after creation
     def after_create_info(self, uuid):
         response = requests.get(baseURL + '/server/' + uuid)
         server_details = response.json()
@@ -304,6 +316,38 @@ class Cli:
         }
         print(json.dumps(dict, indent=4))
 
+    #start the logging process in the backend
+    def logging_thread(self, new_uuid_list):
+        while new_uuid_list:
+            for uuid in new_uuid_list:
+                status = self.get_server_status(uuid)
+                if status != 'maintenance':
+                    new_uuid_list.remove(uuid)
+                    self.mylogger.info_logger('The Server: ' + uuid + ' is in ' + status + ' status.')
+
+    # return creat vm request summary
+    def requestSummary(self, vmDetails, monitor):
+        print("..")
+        summary = []
+        for i in vmDetails:
+            thisdict = {
+                "title": i[5],
+                "hostname": i[0],
+                "zone": i[1],
+                "plan": i[3],
+                "os": i[2],
+                "size": i[4]
+            }
+
+            summary.append(thisdict)
+        print("=======this is your request choices summary======== \n\n\n")
+        print("VMs DETAILS \n\n\n")
+        print(summary)
+        print("\n\n\n")
+        print("MONITORING CHOICE: ", monitor, "\n\n\n")
+        return summary
+
+    # perfrom create vm action
     def performe_CreateVM(self):
         vmDetails = self.get_vm_details()
         monitor = self.request_progress()
@@ -336,14 +380,7 @@ class Cli:
             t = threading.Thread(target=self.logging_thread, args=(new_uuid_list,))
             t.start()
 
-    def logging_thread(self, new_uuid_list):
-        while new_uuid_list:
-            for uuid in new_uuid_list:
-                status = self.get_server_status(uuid)
-                if status != 'maintenance':
-                    new_uuid_list.remove(uuid)
-                    self.mylogger.info_logger('The Server: ' + uuid + ' is in ' + status + ' status.')
-
+    # perfrom modify vm action
     def perform_modify(self):
         uuid_list = []
         out = self.get_choice("multi_choices")
@@ -387,7 +424,7 @@ class Cli:
             response_start = requests.post(baseURL + '/server/start/' +uuid)
             print('The server has been started \n')
 
-
+    # perfrom delete vm action
     def performe_deleteVm(self):
         uuid_list=[]
         out = self.get_choice("multi_choices")
@@ -412,9 +449,11 @@ class Cli:
             else:
                 print("Failed to destroy server (uuid: " + uuid + "): " + json.loads(response.text)['error']['error_message'])
 
+    # perfrom check vm  status  action
     def performe_CheckVmStatus(self):
         self.get_checkStatus_choice()
 
+    # perfrom access vm console action
     def perfome_VmConsole(self):
         uuid = self.get_choice()
         response = requests.get(baseURL + '/server/' + uuid)
@@ -458,8 +497,7 @@ class Cli:
             except KeyboardInterrupt:
                 break
 
-        # add the manager component
-
+    # perfrom check vm performance action
     def perfome_checkPerformance(self):
         uuid = self.get_choice()
         response = requests.get(baseURL + '/server/perf/' + uuid)
@@ -467,6 +505,7 @@ class Cli:
         for line in perf_details:
             print(line.strip())
 
+    # perfrom check  vm events  action
     def perform_events(self):
         uuid = self.get_choice()
         response = requests.get(baseURL + '/logs/' + uuid)
@@ -474,125 +513,30 @@ class Cli:
         for line in logs:
             print(line.strip())
 
+    # execution the action chosen by the user
     def action(self):
         print('#############WELCOME#############')
         action = self.ask_action()
         while True:
-            if (action == 'CreateVM'):
+            if (action == 'Create VM'):
                 self.performe_CreateVM()
-            elif (action == 'CheckVmStatus'):
+            elif (action == 'Check VM Status'):
                 self.performe_CheckVmStatus()
-            elif (action == 'DeleteVm'):
+            elif (action == 'Delete VM'):
                 self.performe_deleteVm()
-            elif (action == 'VmConsole'):
+            elif (action == 'Access VM Console'):
                 self.perfome_VmConsole()
-            elif (action == 'PerformanceStat'):
+            elif (action == 'Check VM Performance'):
                 self.perfome_checkPerformance()
-            elif (action == 'VmEvents'):
+            elif (action == 'Check VM Events'):
                 self.perform_events()
-            elif (action == 'Exit'):
+            elif (action == 'EXIT'):
                 print('########EXITING PROGRAM THANKS##########')
                 exit()
             action = self.ask_action()
 
-    def requestSummary(self, vmDetails, monitor):
-        print("..")
-        summary = []
-        for i in vmDetails:
-            thisdict = {
-                "title": i[5],
-                "hostname": i[0],
-                "zone": i[1],
-                "plan": i[3],
-                "os": i[2],
-                "size": i[4]
-            }
-
-            summary.append(thisdict)
-        print("=======this is your request choices summary======== \n\n\n")
-        print("VMs DETAILS \n\n\n")
-        print(summary)
-        print("\n\n\n")
-        print("MONITORING CHOICE: ", monitor, "\n\n\n")
-        return summary
-
-    def get_server_status(self, uuid):
-        response = requests.get(baseURL + '/server/status/' + uuid)
-        return response.text
-
-    #
-    #
-    # def vm_name_input(self):
-    #     questions = [
-    #         {
-    #             'type': 'input',
-    #             'name': 'vmName',
-    #             'message': 'What\'s your VM name',
-    #         }
-    #     ]
-    #     answers = prompt(questions)
-    #     return answers['vmName']
-    #
-    # def vm_uuid_input(self):
-    #     questions = [
-    #         {
-    #             'type': 'input',
-    #             'name': 'vmName',
-    #             'message': 'What\'s your VM uuid',
-    #         }
-    #     ]
-    #     answers = prompt(questions)
-    #     return answers['vmName']
-    #
-    #
-    # def vm_number_input(self):
-    #     questions = [
-    #         {
-    #             'type': 'input',
-    #             'name': 'vmNumber',
-    #             'message': 'how many VMs you would like to create',
-    #         }
-    #     ]
-    #     answers = prompt(questions)
-    #     return int(answers['vmNumber'])
-
-    def get_input(self, msg):
-        questions = [
-            {
-                'type': 'input',
-                'name': 'x',
-                'message': msg,
-            }
-        ]
-        if 'title' not in msg:
-            while True:
-                answers = prompt(questions)
-                if answers['x']:
-                    break
-                else:
-                    print('Please enter a value.')
-        else:
-            answers = prompt(questions)
-        return answers['x']
-
-    # def encounter2b():
-    #     prompt({
-    #         'type': 'list',
-    #         'name': 'weapon',
-    #         'message': 'Pick one',
-    #         'choices': [
-    #             'Use the stick',
-    #             'Grab a large rock',
-    #             'Try and make a run for it',
-    #             'Attack the wolf unarmed'
-    #         ]
-    #     },  style=style)
-    #     print('The wolf mauls you. You die. The end.')
-
-    # if __name__ == '__main__':
-    #     main()
 
 
 if __name__ == '__main__':
     ins = Cli()
-    ins.perform_modify()
+    ins.action()
